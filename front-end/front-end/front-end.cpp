cc33f30430af554048b89496f6182e5b9fed906d
Kamen Dimanov <hhkimryanov19@codingburgas.bg>
2022-01-31 21:37:40 +0200

    Update the main menu and start to make the "Events" tab

--- front-end/front-end/front-end.cpp
+++ front-end/front-end/front-end.cpp
@@ -1,19 +1,8 @@
 #include<SFML/Graphics.hpp> 
 #include <iostream>
+#include<windows.h>
 using namespace std;
 using namespace sf;
-
-void currentLocation(int* ptr, int n, Event event, Text* textOne)
-{
-	(*textOne).setFillColor(Color::White);
-	if (event.type == Event::MouseButtonPressed)
-	{
-		if (event.mouseButton.button == Mouse::Left)
-		{
-			*ptr = n;
-		}
-	}
-}
 
 void backToMenu(int* ptr1, Event event, Text* textOne)
 {
@@ -34,43 +23,54 @@ void backToMenu(int* ptr1, Event event, Text* textOne)
 	}
 }
 
+void currentLocation(int* ptr, int n, Event event, Text* textOne)
+{
+	(*textOne).setFillColor(Color::White);
+	if (event.type == Event::MouseButtonPressed)
+	{
+		if (event.mouseButton.button == Mouse::Left)
+		{
+			*ptr = n;
+		}
+	}
+}
 int main()
 {
 	int location = 0;
 	RenderWindow window(VideoMode(1000, 800), "Helpopedia");
-	Texture tex1;
-	Sprite s1(tex1);
+	Texture tex1, tex2;
+	Sprite s1(tex1), s2(tex2);
 	Font f1;
 	f1.loadFromFile("Pacifico-Regular.ttf");
 
-	Text text1, text2, text3, text4, text5, text6;
+	Text text1, text2, text3, text4, text5, text, text6;
 	text1.setFont(f1);
 	text1.setCharacterSize(40);
-	text1.setString("Start");
+	text1.setString("Events");
 	text1.setPosition(window.getSize().x / 2 - 50, 500);
 	text1.setFillColor(Color::Black);
 
 	text2.setFont(f1);
 	text2.setCharacterSize(40);
-	text2.setString("Login");
+	text2.setString("Create");
 	text2.setPosition(window.getSize().x / 2 - 50, 550);
 	text2.setFillColor(Color::Black);
 
 	text3.setFont(f1);
 	text3.setCharacterSize(40);
-	text3.setString("Rules");
+	text3.setString("Game");
 	text3.setPosition(window.getSize().x / 2 - 55, 600);
 	text3.setFillColor(Color::Black);
 
 	text4.setFont(f1);
 	text4.setCharacterSize(40);
-	text4.setString("History");
+	text4.setString("About us");
 	text4.setPosition(window.getSize().x / 2 - 65, 650);
 	text4.setFillColor(Color::Black);
 
 	text5.setFont(f1);
 	text5.setCharacterSize(40);
-	text5.setString("Lines");
+	text5.setString("Quit");
 	text5.setPosition(window.getSize().x / 2 - 50, 700);
 	text5.setFillColor(Color::Black);
 
@@ -81,8 +81,33 @@ int main()
 	text6.setFillColor(Color::Color(210, 210, 210, 255));
 
 	tex1.loadFromFile("Logo.png");
+	tex2.loadFromFile("Screenshot 2022-01-30 180704_auto_x2-modified.png");
+
+
+	RectangleShape rectangle(Vector2f(900, 450));
+	rectangle.setFillColor(Color::Black);
+	rectangle.setPosition(50, 50);
+
+	RectangleShape rectangle1(Vector2f(300, 250));
+	rectangle1.setFillColor(Color::Black);
+	rectangle1.setPosition(50, 520);
+
+	RectangleShape rectangle2(Vector2f(200, 250));
+	rectangle2.setFillColor(Color::Black);
+	rectangle2.setPosition(370, 520);
+
+	RectangleShape rectangle3(Vector2f(200, 250));
+	rectangle3.setFillColor(Color::Black);
+	rectangle3.setPosition(590, 520);
+
+	RectangleShape rectangle4(Vector2f(140, 250));
+	rectangle4.setFillColor(Color::Black);
+	rectangle4.setPosition(810, 520);
+
 	s1.setTextureRect(IntRect(10, 10, 486, 500));
+	s2.setTextureRect(IntRect(10, 10, 1300, 1300));
 	s1.setPosition(window.getSize().x / 2 - tex1.getSize().x / 2, 0);
+
 	while (window.isOpen())
 	{
 		Event event;
@@ -91,16 +116,18 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 		}
+
 		window.clear(Color(210, 210, 210, 255));
 		if (location == 0)
 		{
+			window.draw(s2);
 			window.draw(s1);
 			window.draw(text1);
 			window.draw(text2);
 			window.draw(text3);
 			window.draw(text4);
 			window.draw(text5);
-			
+
 			//text1
 			if (Mouse::getPosition().x > 915 && Mouse::getPosition().x < 1025 && Mouse::getPosition().y>685 && Mouse::getPosition().y < 715)
 			{
@@ -157,6 +184,11 @@ int main()
 			{
 			case 1:
 				backToMenu(&location, event, &text6);
+				window.draw(rectangle);
+				window.draw(rectangle1);
+				window.draw(rectangle2);
+				window.draw(rectangle3);
+				window.draw(rectangle4);
 				window.draw(text6);
 				break;
 			case 2:
