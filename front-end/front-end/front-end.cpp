8edca2483089931075ac8aca28676f3c1bbd06f6
Kamen Dimanov <hhkimryanov19@codingburgas.bg>
2022-01-31 20:21:01 +0200

    Add a team logo and menu tabs

--- front-end/front-end/front-end.cpp
+++ front-end/front-end/front-end.cpp
@@ -6,7 +6,39 @@ int main()
 {
 	int location = 0;
 	RenderWindow window(VideoMode(1000, 800), "Helpopedia");
+	Texture tex1;
+	Sprite s1(tex1);
+	Font f1;
+	f1.loadFromFile("Pacifico-Regular.ttf");
 
+	Text text1, text2, text3, text4, text5;
+	text1.setFont(f1);
+	text1.setCharacterSize(40);
+	text1.setString("Start");
+	text1.setFillColor(Color::Black);
+
+	text2.setFont(f1);
+	text2.setCharacterSize(40);
+	text2.setString("Login");
+	text2.setFillColor(Color::Black);
+
+	text3.setFont(f1);
+	text3.setCharacterSize(40);
+	text3.setString("Rules");
+	text3.setFillColor(Color::Black);
+
+	text4.setFont(f1);
+	text4.setCharacterSize(40);
+	text4.setString("History");
+	text4.setFillColor(Color::Black);
+
+	text5.setFont(f1);
+	text5.setCharacterSize(40);
+	text5.setString("Lines");
+	text5.setFillColor(Color::Black);
+
+	tex1.loadFromFile("Logo.png");
+	s1.setTextureRect(IntRect(10, 10, 486, 500));
 	while (window.isOpen())
 	{
 		Event event;
@@ -15,6 +47,13 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 		}
+		window.clear(Color(210, 210, 210, 255));
+		window.draw(s1);
+		window.draw(text1);
+		window.draw(text2);
+		window.draw(text3);
+		window.draw(text4);
+		window.draw(text5);
 		window.display();
 	}
 	return 0;
