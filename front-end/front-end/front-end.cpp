21be73be97a223f72504994a4dc96d6dd72b78b0
Kamen Dimanov <hhkimryanov19@codingburgas.bg>
2022-02-01 14:20:39 +0200

    Add drop-down menu in "Create" tab

--- front-end/front-end/front-end.cpp
+++ front-end/front-end/front-end.cpp
@@ -42,11 +42,11 @@ int main()
 	Font f1;
 	f1.loadFromFile("Pacifico-Regular.ttf");
 
-	Text text1, text2, text3, text4, text5, text, text6;
+	Text text1, text2, text3, text4, text5, text, text6, text7, text8, text9;
 	text1.setFont(f1);
 	text1.setCharacterSize(40);
 	text1.setString("Events");
-	text1.setPosition(window.getSize().x / 2 - 50,500);
+	text1.setPosition(window.getSize().x / 2 - 50, 500);
 	text1.setFillColor(Color::Black);
 
 	text2.setFont(f1);
@@ -74,34 +74,60 @@ int main()
 	text5.setFillColor(Color::Black);
 
 	text6.setFont(f1);
-	text6.setCharacterSize(30);
 	text6.setString("Back");
+	text6.setCharacterSize(30);
 	text6.setPosition(0, 770);
 	text6.setFillColor(Color::Color(210, 210, 210, 255));
+
+
+
+
+	text7.setFont(f1);
+	text7.setCharacterSize(20);
+	text7.setString("Create");
+	text7.setPosition(window.getSize().x / 2 - 50, 100);
+	text7.setFillColor(Color::Color(210, 210, 210, 255));
+
+	text8.setFont(f1);
+	text8.setCharacterSize(20);
+	text8.setString("Delete");
+	text8.setPosition(window.getSize().x / 2 - 50, 140);
+	text8.setFillColor(Color::Color(210, 210, 210, 255));
+
+	text9.setFont(f1);
+	text9.setCharacterSize(20);
+	text9.setString("Stas");
+	text9.setPosition(window.getSize().x / 2 - 50, 180);
+	text9.setFillColor(Color::Color(210, 210, 210, 255));
 
 	tex1.loadFromFile("Logo.png");
 	tex2.loadFromFile("Screenshot 2022-01-30 180704_auto_x2-modified.png");
 
+	int fu1 = 50, fu2 = 90, fu = 2;
 
-	RectangleShape rectangle(Vector2f(900, 450));
+	RectangleShape rectangle(Vector2f(900, 450)), rectangle1(Vector2f(300, 250)),
+		rectangle2(Vector2f(200, 250)), rectangle3(Vector2f(200, 250)),
+		rectangle4(Vector2f(140, 250)), rectangle5(Vector2f(window.getSize().x - 100, 40)), rectangle6(Vector2f(window.getSize().x - 100, 120));
 	rectangle.setFillColor(Color::Black);
 	rectangle.setPosition(50, 50);
 
-	RectangleShape rectangle1(Vector2f(300, 250));
 	rectangle1.setFillColor(Color::Black);
 	rectangle1.setPosition(50, 520);
 
-	RectangleShape rectangle2(Vector2f(200, 250));
 	rectangle2.setFillColor(Color::Black);
 	rectangle2.setPosition(370, 520);
 
-	RectangleShape rectangle3(Vector2f(200, 250));
 	rectangle3.setFillColor(Color::Black);
 	rectangle3.setPosition(590, 520);
 
-	RectangleShape rectangle4(Vector2f(140, 250));
 	rectangle4.setFillColor(Color::Black);
 	rectangle4.setPosition(810, 520);
+
+	rectangle5.setFillColor(Color::Color(150, 150, 150, 255));
+	rectangle5.setPosition(50, 50);
+
+	rectangle6.setFillColor(Color::Color(170, 170, 170, 255));
+	rectangle6.setPosition(50, 90);
 
 	s1.setTextureRect(IntRect(10, 10, 486, 500));
 	s2.setTextureRect(IntRect(10, 10, 1300, 1300));
@@ -127,7 +153,8 @@ int main()
 			window.draw(text5);
 
 			//text1
-			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text1.getCharacterSize()*1.25 && Mouse::getPosition(window).x < window.getSize().x / 2 + text1.getCharacterSize()*1.75 &&
+			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text1.getCharacterSize() * 1.25 &&
+				Mouse::getPosition(window).x < window.getSize().x / 2 + text1.getCharacterSize() * 1.75 &&
 				Mouse::getPosition(window).y > 500 && Mouse::getPosition(window).y < 545)
 			{
 				currentLocation(&location, 1, event, &text1);
@@ -138,7 +165,8 @@ int main()
 			}
 
 			//text2
-			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text2.getCharacterSize() * 1.25 && Mouse::getPosition(window).x < window.getSize().x / 2 + text2.getCharacterSize() * 1.75 &&
+			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text2.getCharacterSize() * 1.25 &&
+				Mouse::getPosition(window).x < window.getSize().x / 2 + text2.getCharacterSize() * 1.75 &&
 				Mouse::getPosition(window).y > 550 && Mouse::getPosition(window).y < 590)
 			{
 				currentLocation(&location, 2, event, &text2);
@@ -149,7 +177,8 @@ int main()
 			}
 
 			//text3
-			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text3.getCharacterSize() * 1.25 && Mouse::getPosition(window).x < window.getSize().x / 2 + text3.getCharacterSize() * 1.75 &&
+			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text3.getCharacterSize() * 1.25 &&
+				Mouse::getPosition(window).x < window.getSize().x / 2 + text3.getCharacterSize() * 1.75 &&
 				Mouse::getPosition(window).y > 600 && Mouse::getPosition(window).y < 640)
 			{
 				currentLocation(&location, 3, event, &text3);
@@ -160,7 +189,8 @@ int main()
 			}
 
 			//text4
-			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text4.getCharacterSize() * 1.25 && Mouse::getPosition(window).x < window.getSize().x / 2 + text4.getCharacterSize() * 1.75 &&
+			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text4.getCharacterSize() * 1.25 &&
+				Mouse::getPosition(window).x < window.getSize().x / 2 + text4.getCharacterSize() * 1.75 &&
 				Mouse::getPosition(window).y > 650 && Mouse::getPosition(window).y < 690)
 			{
 				currentLocation(&location, 4, event, &text4);
@@ -171,7 +201,8 @@ int main()
 			}
 
 			//text5
-			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text5.getCharacterSize() * 1.25 && Mouse::getPosition(window).x < window.getSize().x / 2 + text5.getCharacterSize() * 1.75 &&
+			if (Mouse::getPosition(window).x > window.getSize().x / 2 - text5.getCharacterSize() * 1.25 &&
+				Mouse::getPosition(window).x < window.getSize().x / 2 + text5.getCharacterSize() * 1.75 &&
 				Mouse::getPosition(window).y > 700 && Mouse::getPosition(window).y < 740)
 			{
 				currentLocation(&location, 5, event, &text5);
@@ -195,6 +226,25 @@ int main()
 				window.draw(text6);
 				break;
 			case 2:
+				window.draw(rectangle5);
+				if (Mouse::getPosition(window).x > 50 && Mouse::getPosition(window).x < window.getSize().x - 50 &&
+					Mouse::getPosition(window).y>fu1 && Mouse::getPosition(window).y < fu2)
+				{
+					fu = 1;
+					fu2 = 210;
+					if (fu)
+					{
+						window.draw(rectangle6);
+						window.draw(text7);
+						window.draw(text8);
+						window.draw(text9);
+					}
+				}
+				else
+				{
+					fu = 0;
+					fu2 = 90;
+				}
 				window.draw(text6);
 				backToMenu(&location, event, &text6);
 				break;
@@ -211,7 +261,7 @@ int main()
 				break;
 			}
 		}
-		cout <<Mouse::getPosition(window).y<<" "<<text1.getCharacterSize() << endl;
+		cout << Mouse::getPosition(window).y << " " << text1.getCharacterSize() << endl;
 		window.display();
 	}
 	return 0;
